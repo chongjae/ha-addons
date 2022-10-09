ef60476f4259c7bf8274f3310ed448612f783501
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-10-10 04:00:35 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 
 JS_FILE="bestin_rs485.js"
 CONFIG_PATH=/data/options.json
-SHARE_DIR=/share
+SHARE_DIR=/share/bestin
 
 if [ ! -f $SHARE_DIR/$JS_FILE ]; then
 	echo "[Info] Initializing "$JS_FILE
 
 if [ -f $SHARE_DIR/$JS_FILE ]; then
-	mv $SHARE_DIR/$JS_FILE
+	mv $SHARE_DIR/$JS_FILE $SHARE_DIR/$JS_FILE
   else
 	mkdir $SHARE_DIR
   fi
@@ -16,11 +16,10 @@ if [ -f $SHARE_DIR/$JS_FILE ]; then
 else
 	echo "[Info] Skip initializing "$JS_FILE
 fi
+JS_FILE=/$SHARE_DIR/$JS_FILE
 
 # start server
 echo "[Info] Run Bestin Wallpad with RS485 stand by... "
-
-JS_FILE=/$SHARE_DIR/$JS_FILE
 node $JS_FILE
 
 #while true; do echo "still live"; sleep 1800; done
