58456afe557c381d3735761d880e93b4e73dc7d3
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-10-10 00:51:34 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,16 @@
 #!/bin/sh
 
-CONFIG_PATH=/data/options.json
-SHARE_DIR=/share
+SHARE_DIR=/share/bestin
 
-CUSTOM_FILE=$(jq --raw-output ".customfile" $CONFIG_PATH)
-MODEL=$(jq --raw-output ".model" $CONFIG_PATH)
-TYPE=$(jq --raw-output ".type" $CONFIG_PATH)
-JS_FILE=$MODEL"_"$TYPE"_bestin.js"
-
-if [ -f $SHARE_DIR/$CUSTOM_FILE ]; then
-	echo "[Info] Initializing with Custom file: "$CUSTOM_FILE
-	JS_FILE=$CUSTOM_FILE
-else
-  	if [ ! -f $SHARE_DIR/$JS_FILE ]; then
-		LS_RESULT=`ls $SHARE_DIR | grep bestin`
-		if [ $? -eq 0 ]; then
-			rm $SHARE_DIR/*bestin.js
-		fi
-        cp /js/$MODEL"_"$TYPE".js" $SHARE_DIR/$JS_FILE
-	fi
+if [ ! -f $SHARE_DIR/bestin.js ]; then
+	mkdir $SHARE_DIR
+	mv /bestin.js $SHARE_DIR
 fi
+/makeconf.sh
 
 # start server
-echo "[Info] Bestin Wallpad stand by... : "$JS_FILE
-
-JS_FILE=/$SHARE_DIR/$JS_FILE
-node $JS_FILE
+echo "[Info] Run Bestin Wallpad with RS485"
+cd $SHARE_DIR
+node $SHARE_DIR/bestin.js
 
 #while true; do echo "still live"; sleep 1800; done
