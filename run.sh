ed2500dd5686edc86dc8e1dffbd882d37db64f1b
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-10-10 01:35:18 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,26 @@
-#!/bin/sh 
-  
- JS_FILE="bestin_rs485.js" 
- CONFIG_PATH=/data/options.json 
- RESET=$(jq --raw-output ".reset" $CONFIG_PATH) 
- SHARE_DIR=/share/bestin
-  
- if [ ! -f $SHARE_DIR/$JS_FILE -o "$RESET" = true ]; then 
-         echo "[Info] Initializing "$JS_FILE 
-   else 
-         mkdir $SHARE_DIR 
-   fi 
-         mv /$JS_FILE $SHARE_DIR 
- else 
-         echo "[Info] Skip initializing "$JS_FILE 
- fi 
-  
- JS_FILE=$SHARE_DIR/$JS_FILE 
-  
- # start server 
- echo "[Info] Bestin Wallpad stand by... " 
-  
- node $JS_FILE 
-  
- #while true; do echo "still live"; sleep 1800; done
+#!/bin/sh
+
+JS_FILE="bestin_rs485.js"
+CONFIG_PATH=/data/options.json
+SHARE_DIR=/share
+
+if [ ! -f $SHARE_DIR/$JS_FILE -o "$RESET" = true ]; then
+	echo "[Info] Initializing "$JS_FILE
+else
+  if [ -f $SHARE_DIR/$JS_FILE ]; then
+	mv $SHARE_DIR/$JS_FILE $SHARE_DIR/$JS_FILE
+  else
+	mkdir $SHARE_DIR
+  fi
+        mv /$JS_FILE $SHARE_DIR
+else
+	echo "[Info] Skip initializing "$JS_FILE
+fi
+
+# start server
+echo "[Info] Wallpad Controller stand by... : "$JS_FILE
+
+JS_FILE=/$SHARE_DIR/$JS_FILE
+node $JS_FILE
+
+#while true; do echo "still live"; sleep 1800; done
