e239e9cfe030884a203636462bfd69e9bf10baa4
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-10-10 02:56:59 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ SHARE_DIR=/share
 CUSTOM_FILE=$(jq --raw-output ".customfile" $CONFIG_PATH)
 MODEL=$(jq --raw-output ".model" $CONFIG_PATH)
 TYPE=$(jq --raw-output ".type" $CONFIG_PATH)
-JS_FILE=$MODEL"_"$TYPE"
+JS_FILE=$MODEL"_"$TYPE"_bestin.js.js"
 
 if [ -f $SHARE_DIR/$CUSTOM_FILE ]; then
 	echo "[Info] Initializing with Custom file: "$CUSTOM_FILE
@@ -15,7 +15,7 @@ else
   	if [ ! -f $SHARE_DIR/$JS_FILE ]; then
 		LS_RESULT=`ls $SHARE_DIR
 		if [ $? -eq 0 ]; then
-			rm $SHARE_DIR/
+			rm $SHARE_DIR/*bestin.js.js
 		fi
         cp /js/$MODEL"_"$TYPE".js" $SHARE_DIR/$JS_FILE
 	fi
