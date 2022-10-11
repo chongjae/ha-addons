484dbc7ce67fd4b0dadbca8b6cf11b5b8b262377
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-10-11 12:08:22 +0900

    Update run.sh

--- wallpad/run.sh
+++ wallpad/run.sh
@@ -1,30 +1,25 @@
 #!/bin/sh
-
-CONFIG_PATH=/data/options.json
+ 
+CONFIG_PATH=/data/options.json 
+RESET=$(jq --raw-output ".reset" $CONFIG_PATH) 
 SHARE_DIR=/share/bestin
+  
+if [ ! -f $SHARE_DIR/ipark.js -o "$RESET" = true ]; then 
+         echo "[Info] Initializing ipark.js"
 
-CUSTOM_FILE=$(jq --raw-output ".customfile" $CONFIG_PATH)
-MODEL=$(jq --raw-output ".model" $CONFIG_PATH)
-TYPE=$(jq --raw-output ".type" $CONFIG_PATH)
-JS_FILE=/bestin/wallpad.js"
-
-if [ -f $SHARE_DIR/$CUSTOM_FILE ]; then
-	echo "[Info] Initializing with Custom file: "$CUSTOM_FILE
-	JS_FILE=$CUSTOM_FILE
-else
-  	if [ ! -f $SHARE_DIR/$JS_FILE ]; then
-		LS_RESULT=`ls $SHARE_DIR | grep wallpad`
-		if [ $? -eq 0 ]; then
-			rm $SHARE_DIR/*wallpad.js
-		fi
-        cp /js/bestin.js $SHARE_DIR/$JS_FILE
-	fi
-fi
-
-# start server
-echo "[Info] Run Bestin Wallpad with RS485 stand by... "
-
-JS_FILE=/$SHARE_DIR/$JS_FILE
-node $JS_FILE
-
-#while true; do echo "still live"; sleep 1800; done
+if [ -f $SHARE_DIR/$JS_FILE ]; then 
+       mv $SHARE_DIR/ipark.js $SHARE_DIR/ipark.js 
+else 
+       mkdir $SHARE_DIR 
+   fi 
+       mv /ipark.js $SHARE_DIR 
+ else 
+         echo "[Info] Skip initializing ipark.js"
+ fi 
+  
+ # start server 
+ echo "[Info] Run Bestin Wallpad with RS485 stand by..." 
+  
+ node ipark.js 
+  
+ #while true; do echo "still live"; sleep 1800; done
