329f4e8af2001f4ce72ca7f1b86ba1fbf436b0dd
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-10-14 11:52:01 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
-node ipark.js $
+#!/bin/sh
+
+CONFIG_PATH=/data/options.json
+SHARE_DIR=/share/bestin
+
+if [ i -f $SHARE_DIR/ipark.js ]; then
+fi
+
+# start server
+echo "[Info] Bestin Wallpad with RS485 stand by... "
+
+cd $SHARE_DIR
+node $SHARE_DIR/ipark.js
+
+#while true; do echo "still live"; sleep 1800; done
