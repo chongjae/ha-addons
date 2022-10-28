7f4fbe9dc53b8f81e741a97addc5bf2f0b154311
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-10-29 00:54:36 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
 CONFIG_PATH=/data/options.json
-SHARE_DIR=/share/bestin
+SHARE_DIR=/share
 
-if [ ! -f $SHARE_DIR/ipark.js ]; then
-	mkdir $SHARE_DIR
-	mv ipark.js $SHARE_DIR
+OPTION_FILE="$(jq --raw-output '.custom_file' $CONFIG_PATH)"
+JS_DIR=/js
+JS_FILE="bestin.js"
+
+if [ ! -f $SHARE_DIR/$DEV_FILE ]; then
+  cp $JS_DIR/$JS_FILE $SHARE_DIR/$JS_FILE
+fi
+if [ -f $SHARE_DIR/$OPTION_FILE ]; then
+  echo "[Info] Initializing with Custom file: "$OPTION_FILE
+  if [ "$DEV_FILE" != "$OPTION_FILE" ]; then
+    rm $SHARE_DIR/$DEV_FILE
+    cp $SHARE_DIR/$OPTION_FILE $SHARE_DIR/$DEV_FILE
+  fi
 fi
 
 # start server
-echo "[Info] Bestin Wallpad with RS485 stand by... "
+echo "[Info] Start Wallpad Controller.."
 
-cd $SHARE_DIR
-node $SHARE_DIR/ipark.js
-
-#while true; do echo "still live"; sleep 1800; done
+node $JS_DIR/$JS_FILE
