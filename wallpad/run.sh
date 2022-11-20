988bd19f4153e99c06be663fad24a252f201e664
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-11-20 23:28:26 +0900

    Update run.sh

--- wallpad/run.sh
+++ wallpad/run.sh
@@ -7,14 +7,6 @@ OPTION_FILE="$(jq --raw-output '.custom_file' $CONFIG_PATH)"
 JS_DIR=/js
 JS_FILE="bestin.js"
 
-if [ -f $SHARE_DIR/$OPTION_FILE ]; then
-  echo "[Info] Initializing with Custom file: "$OPTION_FILE
-  if [ "$JS_FILE" != "$OPTION_FILE" ]; then
-    rm $SHARE_DIR/$JS_FILE
-    cp $SHARE_DIR/$OPTION_FILE 
-  fi
-fi
-
 # start server
 echo "[Info] Start Wallpad Controller.."
 
