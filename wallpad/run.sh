2ab41ee804278b2e8fedb747c456673f8de87078
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-11-20 11:12:59 +0900

    Update run.sh

--- wallpad/run.sh
+++ wallpad/run.sh
@@ -3,22 +3,24 @@
 CONFIG_PATH=/data/options.json
 SHARE_DIR=/share
 
-OPTION_FILE="$(jq --raw-output '.custom_file' $CONFIG_PATH)"
-JS_DIR=/js
+CUSTOM_FILE=$(jq --raw-output ".customfile" $CONFIG_PATH)
 JS_FILE="bestin.js"
 
-if [ ! -f $SHARE_DIR/$DEV_FILE ]; then
-  cp $JS_DIR/$JS_FILE $SHARE_DIR/$JS_FILE
-fi
-if [ -f $SHARE_DIR/$OPTION_FILE ]; then
-  echo "[Info] Initializing with Custom file: "$OPTION_FILE
-  if [ "$DEV_FILE" != "$OPTION_FILE" ]; then
-    rm $SHARE_DIR/$DEV_FILE
-    cp $SHARE_DIR/$OPTION_FILE $SHARE_DIR/$DEV_FILE
-  fi
+if [ -f $SHARE_DIR/$CUSTOM_FILE ]; then
+	echo "[Info] Initializing with Custom file: "$CUSTOM_FILE
+	JS_FILE=$CUSTOM_FILE
+else
+  	if [ ! -f $SHARE_DIR/$JS_FILE ]; then
+		LS_RESULT=`ls $SHARE_DIR | grep wallpad`
+		if [ $? -eq 0 ]; then
+			rm $SHARE_DIR/*bestin.js
+		fi
+        cp /js/bestin.js" $SHARE_DIR/$JS_FILE
+	fi
 fi
 
 # start server
 echo "[Info] Start Wallpad Controller.."
 
-node $JS_DIR/$JS_FILE
+JS_FILE=/$SHARE_DIR/$JS_FILE
+node $JS_FILE
