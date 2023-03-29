907b99d4af07ac46127a0432ba9394b726976306
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-03-29 18:24:23 +0900

    Update run.sh

--- wallpad_packets/run.sh
+++ wallpad_packets/run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-share_dir="/share/kocom"
+share_dir="/share/packet"
 
-if [ ! -f "$share_dir/kocom.js" ]; then
+if [ ! -f "$share_dir/data.js" ]; then
     if ! mkdir -p "$share_dir"; then
         echo "ERROR: Failed to create directory $share_dir"
         exit 1
     fi
 
-    if ! mv /kocom.js "$share_dir"; then
-        echo "ERROR: Failed to move kocom.js to $share_dir"
+    if ! mv /data.js "$share_dir"; then
+        echo "ERROR: Failed to move data.js to $share_dir"
         exit 1
     fi
 fi
 
-echo "INFO: Running Daelim Kocom Wallpad Addon..."
+echo "INFO: Running WallPad Packets Addon..."
 cd "$share_dir"
-if ! node kocom.js; then
-    echo "ERROR: Failed to run kocom.js"
+if ! node data.js; then
+    echo "ERROR: Failed to run data.js"
     exit 1
 fi
 
