a8da00e5a089efa99b49026f7ea6ea1c57cf1e27
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-04-02 18:56:49 +0900

    Update run.sh

--- wallpad_packets/run.sh
+++ wallpad_packets/run.sh
@@ -2,12 +2,16 @@
 
 share_dir="/share/packet"
 
-if [ ! -d "$share_dir" ]; then
-    mkdir -p "$share_dir" || exit 1
-fi
-
 if [ ! -f "$share_dir/data.js" ]; then
-    mv /data.js "$share_dir" || exit 1
+    if ! mkdir -p "$share_dir"; then
+        echo "ERROR: Failed to create directory $share_dir"
+        exit 1
+    fi
+
+    if ! mv /data.js "$share_dir"; then
+        echo "ERROR: Failed to move data.js to $share_dir"
+        exit 1
+    fi
 fi
 
 echo "INFO: Running WallPad Packets Addon..."
