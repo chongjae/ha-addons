a086ab524f2d15e82eed7eb761131fa9773a16e3
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-04-12 18:06:16 +0900

    Update run.sh

--- bestin_wallpad/run.sh
+++ bestin_wallpad/run.sh
@@ -1,25 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-share_dir="/share/bestin"
+SHARE_DIR=/share/bestin
 
-if [ ! -f "$share_dir/bestin.js" ]; then
-    if ! mkdir -p "$share_dir"; then
-        echo "ERROR: Failed to create directory $share_dir"
-        exit 1
-    fi
-
-    if ! mv /bestin.js "$share_dir"; then
-        echo "ERROR: Failed to move bestin.js to $share_dir"
-        exit 1
-    fi
+if [ ! -f $SHARE_DIR/bestin.js ]; then
+    mkdir -p $SHARE_DIR
+    mv /bestin.js $SHARE_DIR
 fi
 
-echo "INFO: Running bestin Addon..."
-cd "$share_dir"
-if ! node bestin.js; then
-    echo "ERROR: Failed to run bestin.js"
-    exit 1
-fi
+echo "INFO: Running HDC BESTIN WallPad RS485 Addon..."
+cd $SHARE_DIR
+node bestin.js
 
-# For dev
-# while true; do echo "still live"; sleep 100; done
