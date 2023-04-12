70f4a79d11f2ac48d3e7e8336a8911002d8eefe4
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-04-12 18:07:56 +0900

    Update run.sh

--- bestin_infancy/run.sh
+++ bestin_infancy/run.sh
@@ -1,25 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-share_dir="/share/bestin"
+SHARE_DIR=/share/bestin
 
-if [ ! -f "$share_dir/bestin_infancy.js" ]; then
-    if ! mkdir -p "$share_dir"; then
-        echo "ERROR: Failed to create directory $share_dir"
-        exit 1
-    fi
-
-    if ! mv /bestin_infancy.js "$share_dir"; then
-        echo "ERROR: Failed to move bestin_infancy.js to $share_dir"
-        exit 1
-    fi
+if [ ! -f $SHARE_DIR/bestin_infancy.js ]; then
+    mkdir -p $SHARE_DIR
+    mv /bestin_infancy.js $SHARE_DIR
 fi
 
-echo "INFO: Running bestin Addon..."
-cd "$share_dir"
-if ! node bestin_infancy.js; then
-    echo "ERROR: Failed to run bestin_infancy.js"
-    exit 1
-fi
-
-# For dev
-# while true; do echo "still live"; sleep 100; done
+echo "INFO: Running HDC Infancy BESTIN WallPad RS485 Addon..."
+cd $SHARE_DIR
+node bestin_infancy.js
