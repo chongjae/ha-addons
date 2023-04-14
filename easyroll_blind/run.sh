8c634dde7e328926c796dd6679f7846b8a81d5f0
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-04-14 23:47:55 +0900

    Update run.sh

--- easyroll_blind/run.sh
+++ easyroll_blind/run.sh
@@ -2,12 +2,11 @@
 
 SHARE_DIR=/share/inoshade
 
-if [ ! -f $SHARE_DIR/easyroll_blind.py ]; then
-	mkdir $SHARE_DIR
-	mv /easyroll_blind.py $SHARE_DIR
+if [ ! -f $SHARE_DIR/easyroll_blind.js ]; then
+    mkdir -p $SHARE_DIR
+    mv /easyroll_blind.js $SHARE_DIR
 fi
 
 echo "INFO: Running Easyroll Blind Addon..."
 cd $SHARE_DIR
-python3 $SHARE_DIR/easyroll_blind.py
-
+node easyroll_blind.js
