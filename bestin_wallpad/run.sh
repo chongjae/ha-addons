67222a791ba8bc1f4bb88bc2035e3c650b15bc24
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-04-17 17:43:36 +0900

    Update run.sh

--- bestin_wallpad/run.sh
+++ bestin_wallpad/run.sh
@@ -4,10 +4,10 @@ SHARE_DIR=/share/bestin
 
 if [ ! -f $SHARE_DIR/bestin.js ]; then
     mkdir -p $SHARE_DIR
-    mv /bestin.js $SHARE_DIR
+    mv /bestin_wallpad.js $SHARE_DIR
 fi
 
 echo "INFO: Running HDC BESTIN WallPad RS485 Addon..."
 cd $SHARE_DIR
-node bestin.js
+node bestin_wallpad.js
 
