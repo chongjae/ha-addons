c5fee8fc2cc7cc4095ae94d6c31d68533da69731
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-04-20 19:43:17 +0900

    Add files via upload

---
+++ bestin 1.0 2.0/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SHARE_DIR=/share/bestin
+
+if [ ! -f $SHARE_DIR/bestin.js ]; then
+    mkdir -p $SHARE_DIR
+    mv /bestin_wallpad.js $SHARE_DIR
+fi
+
+if [ ! -f $SHARE_DIR/const.js ]; then
+    mv /const.js $SHARE_DIR
+fi
+
+echo "INFO: Running HDC BESTIN WallPad RS485 Addon..."
+cd $SHARE_DIR
+node bestin_wallpad.js
