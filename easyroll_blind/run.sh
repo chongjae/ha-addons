6d20fd4c8bd722bc08cf48cc04f62c4300972370
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-04-25 13:45:19 +0900

    Update run.sh

--- easyroll_blind/run.sh
+++ easyroll_blind/run.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-SHARE_DIR=/share/easyroll
-
-if [ ! -f $SHARE_DIR/easyroll_blind.py ]; then
-	mkdir $SHARE_DIR
-	mv /easyroll_blind.py $SHARE_DIR
-fi
-
-echo "INFO: Easyroll Blind Add-on ..."
-cd $SHARE_DIR
-python3 $SHARE_DIR/easyroll_blind.py
+python easyroll_blind.py
