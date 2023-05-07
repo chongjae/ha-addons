1e7ee1a4f7efff2bec13eff7243df3723dbb6806
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-05-07 15:11:23 +0900

    Create run.sh

---
+++ easyroll_blind/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SHARE_DIR=/share/easyroll
+
+if [ ! -f $SHARE_DIR/easyroll_blind.js ]; then
+    mkdir -p $SHARE_DIR
+    mv /easyroll_blind.js $SHARE_DIR
+fi
+
+echo "INFO: Running EasyRoll Smart Blind Addon..."
+cd $SHARE_DIR
+node easyroll_blind.js
